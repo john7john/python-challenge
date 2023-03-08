ed3ce2d951982780389e5ca53df9870c940ff889
john7john <john7john>
2023-03-08 00:08:12 -0500

    added few outputs

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,8 +1,34 @@
 import csv
 
-filepath = r"C:\Users\Smooth\Desktop\Weekly Challenges\Python\Instructions\PyBank\Resources\budget_data.csv"
+filepath = r"C:\Users\Smooth\Documents\GitHub\python-challenge\Pybank\Resources\budget_data.csv"
+print("Financial Analysis ")
+print("-----------------------------")
 
 with open(filepath, 'r') as csvf:
+            
 
     csvreader = csv.reader(csvf, delimiter=',')
+    header = next(csvreader)
+    months = 0
+    sum = 0
     
+    for row in csvreader:
+        
+        date = row[0]
+        pl = int(row[1])
+        months +=1
+        sum = pl + sum 
+    avg = months/sum
+    print(f"Total Months :{months}")
+    print(f"Total  :${sum}")
+    print(f"Total  :${avg}")
+
+        
+        # count=0
+        # for item in date:
+        #     count +=1
+       
+    
+# print(f"Total Months :{months(date)}")
+        
+       
