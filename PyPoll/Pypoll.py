72a65798df5f7c241c1fba0ea7feadb259241e55
john7john <john7john>
2023-03-09 17:36:55 -0500

    Final pypoll

--- PyPoll/Pypoll.py
+++ PyPoll/Pypoll.py
@@ -32,25 +32,39 @@ with open(path,"r") as csvf:
         if candidate[2] == can:
             can3.append(can)
 
-    
+    canv1 = len(can1)
+    canv2 = len(can2)
+    canv3 = len(can3)
     can1p = (len(can1)/j)*100
     can2p = (len(can2)/j)*100
     can3p = (len(can3)/j)*100
-    print("%.3f%%" % (can1p))
-    print("%.3f%%" % (can2p))
-    print("%.3f%%" % (can3p))
+    # print("%.3f%%" % (can1p))
+    # print("%.3f%%" % (can2p))
+    # print("%.3f%%" % (can3p))
 
     print(f"Total Votes :{j}")
     print("--------------")    
         
-    print(f"{candidate[0]} : {len(can1)}    "    "%.3f%%" % (can1p))
-    print(f"{candidate[1]} : {len(can2)}    "     "%.3f%%" % (can2p))
-    print(f"{candidate[2]} : {len(can3)}    "    "%.3f%%" % (can3p))
+    print(f"{candidate[0]} : {canv1}    "     "%.3f%%" % (can1p))
+    print(f"{candidate[1]} : {canv2}    "     "%.3f%%" % (can2p))
+    print(f"{candidate[2]} : {canv3}    "    "%.3f%%" % (can3p))
+
+     
 
     print("--------------")   
     
+    
+    if canv1 > canv2 and canv1 > canv3:
+        print(f" Winner is :{candidate[0]} ")
+    
+    if canv2 > canv1 and canv2 > canv3:
+        print(f" Winner is :{candidate[1]} ")
 
-
+    if canv3 > canv2 and canv3 > canv1:
+        print(f" Winner is :{candidate[2]} ")
+    
+    
+        
 
 
     
